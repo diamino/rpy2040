83530cb3ea15a98e9abe83afcd546be3ee8ee421
Diamino <diamino@diamino.nl>
2024-01-29 21:41:34 +0100

    feat: several UART registers

--- rpy2040/peripherals/uart.py
+++ rpy2040/peripherals/uart.py
@@ -9,6 +9,9 @@ UART0_SIZE = 0x1000
 # UART registers
 UARTDR = 0x00
 UARTFR = 0x18
+UARTIBRD = 0x24
+UARTFBRD = 0x28
+UARTCR = 0x30
 
 
 class Uart(MemoryRegionMap):
@@ -16,11 +19,24 @@ class Uart(MemoryRegionMap):
     def __init__(self, base_address: int = UART0_BASE, size: int = UART0_SIZE):
         super().__init__("UART", base_address, size)
         self.uartfr = 0
+        self.uartcr = 1
         self.writehooks[UARTDR] = self.write_uartdr
         self.readhooks[UARTFR] = self.read_uartfr
+        self.readhooks[UARTIBRD] = self.read_uartibrd
+        self.readhooks[UARTFBRD] = self.read_uartfbrd
+        self.readhooks[UARTCR] = self.read_uartcr
 
     def write_uartdr(self, value: int) -> None:
         print(f"UART: Write to data register [{value:#x}/'{chr(value)}']...")
 
     def read_uartfr(self) -> int:
         return self.uartfr
+
+    def read_uartibrd(self) -> int:
+        return 0
+
+    def read_uartfbrd(self) -> int:
+        return 0
+
+    def read_uartcr(self) -> int:
+        return self.uartcr
